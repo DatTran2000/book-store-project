c088f605c3a0401009c56ddf8cc5310b85636371
ADMIN\CONG DAT <dat.tran@gumiviet.com>
2021-11-10 14:24:55 +0700

    feature(social login): add social login feature

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/test', function () {
+Route::get('/', function () {
     return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Google login
+Route::get('/login/google', function () {
+    return Socialite::driver('google')->redirect();
+})->name('login.google');
+
+Route::get('/login/google/callback', function () {
+    $user = Socialite::driver('google')->stateless()->user();
+    return response()->json($user);
+});
+
+// Facebook login
+Route::get('/login/facebook', function () {
+    return Socialite::driver('facebook')->redirect();
+})->name('login.facebook');
+
+Route::get('/login/facebook/callback', function () {
+    $user = Socialite::driver('facebook')->stateless()->user();
+    return response()->json($user);
 });
